acecb562707258c03aba9c6fc10075a2ab2ba150
Benjamin <benjamin.gontard2912@gmail.com>
2017-11-17 15:23:09 +0100

    Final

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
@@ -17,11 +17,13 @@ import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.HBox;
 
 public class Main extends Application {
-    SimpleBooleanProperty visibleProperty = new SimpleBooleanProperty(false);
-
+    boolean visible = false;
+    ToggleButton buttonList;
+    BorderPane root;
+    
     public void start(Stage stage) {
-        
-        BorderPane root = new BorderPane();
+
+        root = new BorderPane();
 
         //Le BorderPane qui sera placé a la racine de notre scene
         BorderPane playerPane = new BorderPane();
@@ -39,29 +41,24 @@ public class Main extends Application {
         Button downPlay = new Button("||");
         Button downNext = new Button(">|");
         downButtons.getChildren().addAll(downPrec, downPlay, downNext);
-        
+
         leftPane.setTop(upButtons);
         leftPane.setBottom(downButtons);
-        
+
         BorderPane centerPane = new BorderPane();
-        
+
         BorderPane downSliderAndButtons = new BorderPane();
         Slider soundSlider = new Slider();
         HBox rightButtons = new HBox();
         Button buttonBars = new Button("|||");
-        ToggleButton buttonList = new ToggleButton(":=");
-        buttonList.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                visibleProperty.setValue(!visibleProperty.getValue());
-            }
-        });
-        rightButtons.getChildren().addAll(buttonBars,buttonList);
+        buttonList = new ToggleButton(":=");
+
+        rightButtons.getChildren().addAll(buttonBars, buttonList);
         downSliderAndButtons.setLeft(soundSlider);
         downSliderAndButtons.setRight(rightButtons);
-        
+
         BorderPane upPaneAndSlider = new BorderPane();
-        
+
         Slider videoSlider = new Slider();
         BorderPane nameAndTime = new BorderPane();
         Label name = new Label("Lecteur multimédia VLC");
@@ -70,35 +67,66 @@ public class Main extends Application {
         nameAndTime.setRight(time);
         upPaneAndSlider.setTop(nameAndTime);
         upPaneAndSlider.setBottom(videoSlider);
-        
+
         centerPane.setTop(upPaneAndSlider);
         centerPane.setBottom(downSliderAndButtons);
-        
+
         playerPane.setLeft(leftPane);
         playerPane.setCenter(centerPane);
-        
+
         BorderPane listPane = new BorderPane();
-        listPane.visibleProperty().bind(visibleProperty);
-                
-        
-        
+
+
         BorderPane paneListDown = new BorderPane();
         HBox listButtons = new HBox();
         Button listPlus = new Button("+");
         Button listArrow = new Button("->");
         Button listLoop = new Button("-><-");
-        listButtons.getChildren().addAll(listPlus,listArrow,listLoop);
+        listButtons.getChildren().addAll(listPlus, listArrow, listLoop);
         paneListDown.setLeft(listButtons);
-        paneListDown.setRight(new TextField());
-        
-        listPane.setCenter(new TreeTableView());
+
+        TextField textfield = new TextField();
+        paneListDown.setRight(textfield);
+
+        TreeTableView<Integer> tree = new TreeTableView<>();
+        listPane.setCenter(tree);
         listPane.setBottom(paneListDown);
-        
+        listPane.setVisible(visible);
+        stage.setHeight(90);
         
         root.setTop(playerPane);
         root.setCenter(listPane);
         
 
+        buttonList.setOnAction((ActionEvent event) -> {
+            visible = !visible;
+            if(!visible){
+                stage.setHeight(90);
+                buttonList.setStyle("");
+            }else{
+                stage.setHeight(400);
+                buttonList.setStyle("-fx-base: cyan");
+            }
+            root.requestFocus();
+            listPane.setVisible(visible);
+        });
+
+        stage.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            if (newValue.intValue() < 150){
+                visible = false;
+                stage.setHeight(90);
+                buttonList.setSelected(false);
+                buttonList.setStyle("");
+                
+            } else{
+                visible = true;
+                buttonList.setSelected(true);
+                buttonList.setStyle("-fx-base: cyan");
+            }
+            root.requestFocus();
+            listPane.setVisible(visible);
+        });
+        
         stage.setTitle("Player");
         stage.setScene(new Scene(root));
         stage.show();
